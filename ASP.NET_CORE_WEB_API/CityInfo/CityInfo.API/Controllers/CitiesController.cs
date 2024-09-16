0c79816c886d86f4120e164daed09b87f017f115
Aleksandar Iliev <aleksandriliev8@gmail.com>
2024-09-16 19:06:50 +0300

    Improving the architecture with model classes

--- ASP.NET_CORE_WEB_API/CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ ASP.NET_CORE_WEB_API/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -9,12 +9,13 @@ namespace CityInfo.API.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(
-                new List<object>
-                {
-                    new {id = 1, name = "New York City"},
-                    new {id = 2, name = "Antwerp" }
-                });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
         }
     }
 }
