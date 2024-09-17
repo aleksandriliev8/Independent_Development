eb90a1759a615f51da58679ffe83f6e75f3ee4b1
Aleksandar Iliev <aleksandriliev8@gmail.com>
2024-09-17 03:03:50 +0300

    Validating input

--- ASP.NET_CORE_WEB_API/CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ ASP.NET_CORE_WEB_API/CityInfo/CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae804509fd6d3c8f9a5519606361355040aa6b47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b18ccfdc3299664e32e8a97466443e4a1f8b928e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
