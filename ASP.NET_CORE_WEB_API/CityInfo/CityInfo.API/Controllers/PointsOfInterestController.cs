18ed749e46bf3266cd38d6f9d2d9516557b2f9c5
Aleksandar Iliev <aleksandriliev8@gmail.com>
2024-09-21 01:44:42 +0300

    Updating resource added

--- ASP.NET_CORE_WEB_API/CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ ASP.NET_CORE_WEB_API/CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,5 +1,6 @@
 ﻿using CityInfo.API.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -47,11 +48,11 @@ namespace CityInfo.API.Controllers
 
         [HttpPost]
         public ActionResult<PointOfInterestDto> CreatePointOfInterest(
-            int cityId, 
+            int cityId,
             PointOfInterestForCreationDto pointOfInterest)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
-            if(city == null)
+            if (city == null)
             {
                 return NotFound();
             }
@@ -75,6 +76,32 @@ namespace CityInfo.API.Controllers
                     pointsofinterestid = finalPointOfInterest.Id
                 },
                 finalPointOfInterest);
+        }
+
+        [HttpPut("{pointofinterestid}")]
+
+        public ActionResult UpdatePointOfInterest (int cityId, int pointOfInterestId,
+            PointOfInterestForUpdateDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault (c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            //find point of interest
+            var pointOfInterestFromStore = city.PointsOfInterest
+                .FirstOrDefault(c => c.Id == pointOfInterestId);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
         }
     }
 }
